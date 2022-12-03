8011221184f6ed4fe9cd98dad73a4d1d34522559
krpraveen0 <pkec151015@gmail.com>
2022-12-03 22:12:10 +0530

    even-num solution

---
+++ BTS/sum-even-nos.js
@@ -0,0 +1,11 @@
+const list = [4,6,9,11,21,34,57,89,36,80,25,78,17,46,30]
+
+function even_sum(list){
+    
+    const sum_of_evens = null;
+    //write your logic & store into sum_of_evens variable 
+
+    
+    return sum_of_evens;
+
+}
